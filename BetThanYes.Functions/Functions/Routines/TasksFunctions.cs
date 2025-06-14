17d37c1a5b3a4c78365cb17236680c8b9bc959b5
nombre <valegrey@hotmail.com>
2025-06-13 18:17:33 -0600

    se agrega funcion de tareas

--- BetThanYes.Functions/Functions/Routines/TasksFunctions.cs
+++ BetThanYes.Functions/Functions/Routines/TasksFunctions.cs
@@ -2,34 +2,34 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using BetThanYes.Application.Services.Interfaces;
-using BetThanYes.Domain.DTOs.Request.Routine;
+using BetThanYes.Domain.DTOs.Request.Peticion;
 using BetThanYes.Domain.Models;
-using BetThanYes.Domain.DTOs.Response.Routine;
+using BetThanYes.Domain.DTOs.Response.Peticion;
 using BetThanYes.Functions.Functions.Login;
 using Microsoft.Extensions.Logging;
 
-namespace BetThanYes.Functions.Functions.Routines
+namespace BetThanYes.Functions.Functions.Peticiones
 {
-    public class RoutinesFunction
+    public class PeticionesFunction
     {
-        private readonly IRoutineService _routineService;
+        private readonly IPeticionService _routineService;
         private readonly ILogger<LoginFunction> _logger;
 
-        public RoutinesFunction(ILogger<LoginFunction> logger, IRoutineService routineService)
+        public PeticionesFunction(ILogger<LoginFunction> logger, IRoutineService routineService)
         {
-            _routineService = routineService;
+            _peticionService = routineService;
             _logger = logger;
         }
 
-        [Function("CreateRoutine")]
-        public async Task<ApiResponse<CreateRoutineResponse>> CreateRoutine(
+        [Function("CreatePeticion")]
+        public async Task<ApiResponse<CreatePeticionResponse>> CreateRoutine(
             [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
-            var response = new ApiResponse<CreateRoutineResponse>();
+            var response = new ApiResponse<CreatePeticionResponse>();
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             try
             {
-                var requestBody = await req.ReadFromJsonAsync<CreateRoutineDto>();
+                var requestBody = await req.ReadFromJsonAsync<CreatePeticionDto>();
 
                 if (requestBody == null)
                 {
@@ -39,9 +39,9 @@ namespace BetThanYes.Functions.Functions.Routines
                     return response;
                 }
 
-                var result = await _routineService.CreateAsync(requestBody);
+                var result = await _peticionService.CreateAsync(requestBody);
 
-                response.Data = new CreateRoutineResponse();
+                response.Data = new CreatePeticionResponse();
                 response.Data.Id = result.Id;
                 response.Success = true;
                 response.Message = "Rutina creada exitosamente.";
@@ -60,15 +60,15 @@ namespace BetThanYes.Functions.Functions.Routines
         
         
 
-        [Function("UpdateRoutine")]
-        public async Task<ApiResponse<UpdateRoutineResponse>> UpdateRoutine(
+        [Function("UpdatePeticion")]
+        public async Task<ApiResponse<UpdatePeticionResponse>> UpdateRoutine(
             [HttpTrigger(AuthorizationLevel.Function, "put")] HttpRequestData req)
         {
-            var response = new ApiResponse<UpdateRoutineResponse>();
+            var response = new ApiResponse<UpdatePeticionResponse>();
             _logger.LogInformation("C# HTTP trigger function processed an update request.");
             try
             {
-                var requestBody = await req.ReadFromJsonAsync<UpdateRoutineDto>();
+                var requestBody = await req.ReadFromJsonAsync<UpdatePeticionDto>();
 
                 if (requestBody == null)
                 {
@@ -78,7 +78,7 @@ namespace BetThanYes.Functions.Functions.Routines
                     return response;
                 }
 
-                var routineResponse = await _routineService.UpdateAsync(requestBody);
+                var routineResponse = await _peticionService.UpdateAsync(requestBody);
 
                 response.Data = routineResponse;
                 response.Success = true;
@@ -97,23 +97,23 @@ namespace BetThanYes.Functions.Functions.Routines
         }
         
         
-        [Function("DeleteRoutine")]
-        public async Task<ApiResponse<DeleteRoutineResponse>> DeleteRoutine(
-            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "routines/{id:guid}")] HttpRequestData req, Guid id)
+        [Function("DeletePeticion")]
+        public async Task<ApiResponse<DeletePeticionResponse>> DeletePeticion(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "peticiones/{id:guid}")] HttpRequestData req, Guid id)
         {
-            var response = new ApiResponse<DeleteRoutineResponse>();
+            var response = new ApiResponse<DeletePeticionResponse>();
             _logger.LogInformation("C# HTTP trigger function processed a delete request.");
             try
             {
                 // Llamar al servicio para eliminar la rutina
                 await _routineService.DeleteAsync(id);
-                DeleteRoutineResponse routineResponse = new DeleteRoutineResponse
+                DeletePeticionResponse peticionResponse = new DeletePeticionResponse
                 {
                     Success = true,
                 };
-                response.Data = routineResponse;
+                response.Data = peticionResponse;
                 response.Success = true;
-                response.Message = "Rutina eliminada exitosamente.";
+                response.Message = "Peticion eliminada exitosamente.";
                 response.StatusCode = StatusCodes.Status200OK;
 
                 return response;
@@ -128,22 +128,22 @@ namespace BetThanYes.Functions.Functions.Routines
         }
         
         
-        [Function("GetAllRoutinesByUser")]
-        public async Task<ApiResponse<IEnumerable<Routine>>> GetAllRoutinesByUser(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "routines")] HttpRequestData req)
+        [Function("GetAllPeticionesByUser")]
+        public async Task<ApiResponse<IEnumerable<Peticion>>> GetAllPeticionesByUser(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "peticiones")] HttpRequestData req)
         {
-            var response = new ApiResponse<IEnumerable<Routine>>();
-            _logger.LogInformation("C# HTTP trigger function processed a get all routines request.");
+            var response = new ApiPeticion<IEnumerable<Peticion>>();
+            _logger.LogInformation("C# HTTP trigger function processed a get all peticiones request.");
             try
             {
                 var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
                 Guid userId = new Guid(query["Id"] ?? "");
-                // Llamar al servicio para obtener todas las rutinas
+                // Llamar al servicio para obtener todas las peticiones
                 var routines = await _routineService.GetAllByUserIdAsync(userId); // Cambia Guid.Empty si necesitas un filtro específico
 
-                response.Data = routines;
+                response.Data = peticiones;
                 response.Success = true;
-                response.Message = "Rutinas obtenidas exitosamente.";
+                response.Message = "Peticiones obtenidas exitosamente.";
                 response.StatusCode = StatusCodes.Status200OK;
 
                 return response;
