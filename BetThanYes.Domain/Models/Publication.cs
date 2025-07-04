063d1be7ffc3d0c5d006a87425c9a8a90901c224
Andres Botina <cacbotina@gmail.com>
2025-07-03 22:08:24 -0500

    Se agrega metodo update

--- BetThanYes.Domain/Models/Publication.cs
+++ BetThanYes.Domain/Models/Publication.cs
@@ -6,12 +6,12 @@ namespace BetThanYes.Domain.Models
     public class Publication
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
+        public string? Title { get; set; }
+        public string? Body { get; set; }
         public DateTime CreatedDate { get; set; }
         public Guid UserId { get; set; }  // Clave foránea
         public int CategoryId { get; set; }  // Clave foránea
-
+    
 
     }
 }
